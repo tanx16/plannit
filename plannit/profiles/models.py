f6bb0f809e43802b4bf103b180d9aae6563e0884
alberttjin <albertjin@berkeley.edu>
2017-10-07 01:54:49 -0700

    imagefield not working

--- plannit/profiles/models.py
+++ plannit/profiles/models.py
@@ -5,5 +5,21 @@ class person(models.Model):
     name = models.CharField(max_length = 100)
     bio = models.CharField(max_length = 1000)
     hometown = models.CharField(max_length = 100)
+    pic = models.ImageField()
     def __str__(self):
         return self.name
+
+class schedules(models.Model):
+    user = models.ForeignKey(person, on_delete=models.CASCADE)
+    place = models.CharField(max_length = 100)
+    date = models.DateField(auto_now_add = True)
+    def __str__(self):
+        return self.date
+
+class events(models.Model):
+    start = models.TimeField()
+    end = models.TimeField()
+    title = models.CharField(max_length = 100)
+    location = models.CharField(max_length = 100)
+    def __str__(self):
+        return self.title
--- plannit/profiles/views.py
+++ plannit/profiles/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import Http404
+from django.http import HttpResponse
+from .models import *
 
 # Create your views here.
+def index(request):
+    return
+
+def loadprof(request, profile_id):
+    try:
+        user = person.objects.get(id=profile_id)
+    except person.DoesNotExist:
+        raise Http404("The profile you are looking for does not exist.")
+    return render(request, 'profile.html', {'user': user})

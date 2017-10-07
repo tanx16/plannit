f209fcb7bc000b6c4693f25a35216f1e5435def0
Xufei Tan <tanx@berkeley.edu>
2017-10-07 16:26:22 -0700

    Django is magic (schedule inheritance may work)

--- plannit/profiles/views.py
+++ plannit/profiles/views.py
@@ -89,18 +89,17 @@ class ScheduleFormView(View):
 
 class EventFormView(View):
     form_class = eventForm
-    template_name = 'new_schedule.html'
+    template_name = 'schedule.html'
 
     def get(self, request):
         form = self.form_class()
         return render(request, self.template_name, {'form': form})
 
     def post(self, request):
-
-
+        form = self.form_class(request.POST)
         if form.is_valid():
-            #TODO: Add schedule info from parent 
             event  = form.save(commit = False)
+            schedule = event.schedule #Does this work?
             title  = form.cleaned_data['title']
             start = form.cleaned_data['start']
             end  = form.cleaned_data['end']

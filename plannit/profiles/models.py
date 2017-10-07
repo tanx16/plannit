fdb511e1b86893297a5ebbef5a975ab58d385556
alberttjin <albertjin@berkeley.edu>
2017-10-06 22:22:29 -0700

    added profiles, city

--- plannit/profiles/models.py
+++ plannit/profiles/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class person(models.Model):
+    name = models.CharField(max_length = 100)
+    bio = models.CharField(max_length = 1000)
+    hometown = models.CharField(max_length = 100)
+    def __str__(self):
+        return self.name

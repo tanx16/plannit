3b562ca4726af63d9c2f38cc93672f70c833a999
Xufei Tan <tanx@berkeley.edu>
2017-10-07 14:13:24 -0700

    new branch, new me

--- plannit/profiles/forms.py
+++ plannit/profiles/forms.py
@@ -1,8 +1,16 @@
 from django.contrib.auth.models import User
 from django import forms
 
+from plannit.models import events
+
 class regForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput)
     class Meta:
         model = User
         fields = ['first_name', 'last_name', 'username', 'email', 'password']
+
+class eventForm(forms.ModelForm):
+    class Meta:
+        model = events
+        fields = ['schedule', 'start', 'end', 'title', 'location']
+

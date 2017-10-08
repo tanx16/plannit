cfd5e24dfba59fb83c40dd64c97f8d79de924d17
alberttjin <albertjin@berkeley.edu>
2017-10-08 01:32:06 -0700

    adsf

--- plannit/profiles/views.py
+++ plannit/profiles/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth.views import login
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate, login
 from django.views import generic
-from django.views.generic import View
+from django.views.generic import View, DetailView
 from .forms import regForm, UserForm, PersonForm, scheduleForm, eventForm, LoginForm
 from django.contrib.auth import logout
 from .models import *
@@ -121,20 +121,22 @@ class ScheduleFormView(View):
 
         return render(request, self.template_name, {'form': form})
 
-class EventFormView(View):
+class EventFormView(DetailView):
     form_class = eventForm
     template_name = 'newevent.html'
 
-    def get(self, request):
+    def get(self, request, schedule_id):
         form = self.form_class()
         return render(request, self.template_name, {'form': form})
 
-    def post(self, request):
+    def post(self, request, schedule_id):
         form = self.form_class(request.POST)
         if form.is_valid():
             sid = self.kwargs['schedule_id']
-            event  = form.save(commit = False)
-            schedule = schedules.objects.get(id=sid) 
+            event = form.save(commit = False)
+            schedule = schedules.objects.get(id=sid)
+            event.schedule = schedule
             event.save()
-
-        return render(request, self.template_name, {'form': form, 'events': schedule.events_set.all()})
+            print(schedule.events_set.all())
+            return render(request, self.template_name, {'form': form, 'event':schedule.events_set.all()})
+        return render(request, self.template_name, {'form': form,})

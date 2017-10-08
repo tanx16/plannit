e4d4670ce84d4df1510aeb1a2ae413159ca6c093
Henry Muller <henrymuller@berkeley.edu>
2017-10-08 02:44:38 -0700

    hooked some stuff into backend idk

--- plannit/index/urls.py
+++ plannit/index/urls.py
@@ -3,4 +3,5 @@ from django.contrib.auth.views import login
 from . import views
 urlpatterns = [
     url(r'^$', views.index, name="index"),
+    url(r'^profiles/', include('profiles.urls'), name="profiles"),
 ]
